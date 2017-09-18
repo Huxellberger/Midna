2be388e8e9836940999d36c8d1082bddbd3fc3c6
Jake Huxell <darkduckmovies@gmail.com>
2017-09-18 14:55:27 +0100

    Adding failing test to check the cloud build catches it.

---
+++ Assets/Editor/UnitTests/DefaultTests.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+namespace Midna.Editor.UnitTests
+{
+    [TestFixture]
+    public class DefaultTests
+    {
+        [Test]
+        public void GivenFailingTest_BuildGoesRed()
+        {
+            //Given
+            var gameObject = new GameObject();
+
+            //When
+            //Try to rename the GameObject
+            var newGameObjectName = "My game object";
+            gameObject.name = newGameObjectName;
+
+            //Then
+            //The object has a new name
+            // Make it fail to check build fails
+            Assert.AreNotEqual(newGameObjectName, gameObject.name);
+        }
+    }
+}
