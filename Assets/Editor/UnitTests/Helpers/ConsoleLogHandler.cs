ea082ad3df158a2e921a9a8bb7f22be70697db18
Jake Huxell <darkduckmovies@gmail.com>
2017-09-19 15:02:18 +0100

    Console Log Handler to catch Unity errors

---
+++ Assets/Editor/UnitTests/Helpers/ConsoleLogHandler.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (C) Threetee Gang 2017
+
+using UnityEngine;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Midna.Editor.UnitTests.TestHelpers
+{
+    public class ConsoleLogHandler
+    {
+        public class ConsoleLogEntry
+        {
+            public ConsoleLogEntry(string inErrorMessage)
+            {
+                errorMessage = inErrorMessage;
+                active = true;
+            }
+
+            public string errorMessage { get; set; }
+            public bool active { get; set; }
+        }
+
+        public ConsoleLogHandler()
+        {
+            receivedErrors = new List<ConsoleLogEntry>();
+        }
+
+        public void Activate()
+        {
+            Application.logMessageReceived += HandleLog;
+        }
+
+        public void Deactivate()
+        {
+            if (GetNumberOfActiveErrors() > 0)
+            {
+                Assert.Fail( "Still had active errors on test run completion!" );
+            }
+            Application.logMessageReceived -= HandleLog;
+        }
+
+        public bool HasReceivedNewErrorString(string desiredError)
+        {
+            foreach (var receivedError in receivedErrors)
+            {
+                if (receivedError.errorMessage.Equals(desiredError) && receivedError.active)
+                {
+                    receivedError.active = false;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasReceivedAnyNewErrors()
+        {
+            if (GetNumberOfActiveErrors() > 0)
+            {
+                DeactivateAllActiveErrors();
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasReceivedExactlyNNewErrors(int errorCount)
+        {
+            if (GetNumberOfActiveErrors() == errorCount)
+            {
+                DeactivateAllActiveErrors();
+                return true;
+            }
+
+            return false;
+        }
+
+        private int GetNumberOfActiveErrors()
+        {
+            int totalActiveErrors = 0;
+            foreach (var receivedError in receivedErrors)
+            {
+                if (receivedError.active)
+                {
+                    totalActiveErrors++;
+                }
+            }
+            return totalActiveErrors;
+        }
+
+        private void DeactivateAllActiveErrors()
+        {
+            foreach (var receivedError in receivedErrors)
+            {
+                receivedError.active = false;
+            }
+        }
+
+        private void HandleLog(string logString, string stackTrace, LogType givenLogType)
+        {
+            if (givenLogType == LogType.Error || givenLogType == LogType.Exception)
+            {
+                receivedErrors.Add(new ConsoleLogEntry(logString));
+            }
+        }
+
+        private List<ConsoleLogEntry> receivedErrors;
+    }
+}
---
+++ Assets/Editor/UnitTests/Helpers/ConsoleLogHandlerTests.cs
@@ -0,0 +1,194 @@
+﻿// Copyright (C) Threetee Gang 2017
+
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Midna.Editor.UnitTests.TestHelpers
+{
+    [TestFixture]
+    public class ConsoleLogHandlerTestFixture
+    {
+        [SetUp]
+        public void BeforeTest()
+        {
+            testHandler = new ConsoleLogHandler();
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            testHandler = null;
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_WhenCreated_NotListeningForEvents()
+        {
+            Debug.LogError("TestError");
+
+            Assert.IsFalse(testHandler.HasReceivedAnyNewErrors());
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_WhenActivated_RemembersIfErrorOccurred()
+        {
+            testHandler.Activate();
+
+            Debug.LogError("TestError");
+
+            Assert.IsTrue(testHandler.HasReceivedAnyNewErrors());
+
+            testHandler.Deactivate();
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_WhenActivated_CatchesCorrectError()
+        {
+            testHandler.Activate();
+
+            const string errorMessage = "TestError";
+
+            Debug.LogError(errorMessage);
+
+            Assert.IsTrue(testHandler.HasReceivedNewErrorString(errorMessage));
+            testHandler.Deactivate();
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_WhenActivated_CatchesMultipleErrors()
+        {
+            testHandler.Activate();
+
+            Debug.LogError("TestError");
+            Debug.LogError("OtherTestError");
+
+            Assert.IsTrue(testHandler.HasReceivedExactlyNNewErrors(2));
+            testHandler.Deactivate();
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_WhenDeactivated_StopsCatchingErrors()
+        {
+            testHandler.Activate();
+            testHandler.Deactivate();
+
+            Debug.LogError("TestError");
+
+            Assert.IsFalse(testHandler.HasReceivedAnyNewErrors());
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_HasReceivedAnyNewErrors_NoErrors_False()
+        {
+            testHandler.Activate();
+
+            Assert.IsFalse(testHandler.HasReceivedAnyNewErrors());
+            testHandler.Deactivate();
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_HasReceivedAnyNewErrors_NewError_True()
+        {
+            testHandler.Activate();
+
+            Debug.LogError("TestError");
+
+            Assert.IsTrue(testHandler.HasReceivedAnyNewErrors());
+            testHandler.Deactivate();
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_HasReceivedAnyNewErrors_OldError_False()
+        {
+            testHandler.Activate();
+
+            Debug.LogError("TestError");
+
+            testHandler.HasReceivedAnyNewErrors();
+
+            Assert.IsFalse(testHandler.HasReceivedAnyNewErrors());
+            testHandler.Deactivate();
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_HasReceivedExactlyNNewErrors_WrongN_False()
+        {
+            testHandler.Activate();
+
+            Debug.LogError("TestError");
+
+            Assert.IsFalse(testHandler.HasReceivedExactlyNNewErrors(2));
+
+            testHandler.HasReceivedExactlyNNewErrors(1);
+            testHandler.Deactivate();
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_HasReceivedExactlyNNewErrors_CorrectN_True()
+        {
+            testHandler.Activate();
+
+            Debug.LogError("TestError");
+
+            Assert.IsTrue(testHandler.HasReceivedExactlyNNewErrors(1));
+            testHandler.Deactivate();
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_HasReceivedExactlyNNewErrors_OldErrors_False()
+        {
+            testHandler.Activate();
+
+            Debug.LogError("TestError");
+
+            testHandler.HasReceivedExactlyNNewErrors(1);
+
+            Assert.IsFalse(testHandler.HasReceivedExactlyNNewErrors(1));
+            testHandler.Deactivate();
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_HasReceivedNewErrorOfString_WrongString_False()
+        {
+            testHandler.Activate();
+
+            const string errorMessage = "TestError";
+            const string wrongErrorMessage = "NotTestError";
+
+            Debug.LogError(errorMessage);
+
+            Assert.IsFalse(testHandler.HasReceivedNewErrorString(wrongErrorMessage));
+
+            testHandler.HasReceivedNewErrorString(errorMessage);
+            testHandler.Deactivate();
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_HasReceivedNewErrorOfString_CorrectString_True()
+        {
+            testHandler.Activate();
+
+            const string errorMessage = "TestError";
+
+            Debug.LogError(errorMessage);
+
+            Assert.IsTrue(testHandler.HasReceivedNewErrorString(errorMessage));
+            testHandler.Deactivate();
+        }
+
+        [Test]
+        public void GivenConsoleLogHandler_HasReceivedNewErrorOfString_OldCorrectString_False()
+        {
+            testHandler.Activate();
+
+            const string errorMessage = "TestError";
+
+            Debug.LogError(errorMessage);
+
+            testHandler.HasReceivedNewErrorString(errorMessage);
+            Assert.IsFalse(testHandler.HasReceivedNewErrorString(errorMessage));
+            testHandler.Deactivate();
+        }
+
+        private ConsoleLogHandler testHandler;
+    }
+}
