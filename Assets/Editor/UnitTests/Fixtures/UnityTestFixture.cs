e2669557743675864a2cb14e785703aa017a8fc3
Jake Huxell <darkduckmovies@gmail.com>
2017-09-19 15:20:04 +0100

    Adding UnityTestFixture to automatically setup a console log handler before a test!

---
+++ Assets/Editor/UnitTests/Fixtures/UnityTestFixture.cs
@@ -0,0 +1,33 @@
+﻿// Copyright Threetee Gang (C) 2017
+
+using NUnit.Framework;
+using Midna.Editor.UnitTests.TestHelpers;
+
+namespace Midna.Editor.UnitTests.TestFixtures
+{
+    [TestFixture]
+    public abstract class UnityTestFixture
+    {
+        [SetUp]
+        public void BeforeTest()
+        {
+            consoleLogHandler = new ConsoleLogHandler();
+            consoleLogHandler.Activate();
+
+            OnBeforeTest();
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            OnAfterTest();
+
+            consoleLogHandler.Deactivate();
+        }
+
+        protected abstract void OnBeforeTest();
+        protected abstract void OnAfterTest();
+
+        protected ConsoleLogHandler consoleLogHandler;
+    }
+}
---
+++ Assets/Editor/UnitTests/Fixtures/UnityTestFixtureTests.cs
@@ -0,0 +1,44 @@
+﻿// Copyright Threetee Gang (C) 2017
+
+using UnityEngine;
+using NUnit.Framework;
+
+namespace Midna.Editor.UnitTests.TestFixtures
+{
+    [TestFixture]
+    public class UnityTestFixtureTests
+        : UnityTestFixture
+    {
+        public UnityTestFixtureTests()
+        {
+            onBeforeTestCalled = false;
+        }
+
+        protected override void OnBeforeTest()
+        {
+            onBeforeTestCalled = true;
+        }
+
+        protected override void OnAfterTest()
+        {
+            // Reset
+            onBeforeTestCalled = false;
+        }
+
+        [Test]
+        public void WhenFixtureStartsTest_ConsoleLogHandlerIsActive()
+        {
+            Debug.LogError("Error is here!");
+
+            Assert.IsTrue(consoleLogHandler.HasReceivedExactlyNNewErrors(1));
+        }
+
+        [Test]
+        public void WhenFixtureStartsTest_OnBeforeTestCalled()
+        {
+            Assert.IsTrue(onBeforeTestCalled);
+        }
+
+        private bool onBeforeTestCalled;
+    }
+}
