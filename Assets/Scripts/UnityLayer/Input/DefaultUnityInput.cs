977c83898cd5d9a73273140cfc6cdf710a0e7678
Jake Huxell <darkduckmovies@gmail.com>
2017-09-24 17:31:09 +0100

    DefaultUnityInput

---
+++ Assets/Scripts/UnityLayer/Input/DefaultUnityInput.cs
@@ -0,0 +1,27 @@
+﻿// Copyright Threetee Gang (C) 2017
+
+using UnityEngine;
+
+namespace Assets.Scripts.UnityLayer.Input
+{
+    public class DefaultUnityInput
+        : IUnityInputInterface
+    {
+        // IUnityInputInterface
+        public float GetAxis(string axisName)
+        {
+            return UnityEngine.Input.GetAxis(axisName);
+        }
+
+        public bool GetButton(string buttonName)
+        {
+            return UnityEngine.Input.GetButton(buttonName);
+        }
+
+        public Vector3 GetMousePosition()
+        {
+            return UnityEngine.Input.mousePosition;
+        }
+        // ~IUnityInputInterface
+    }
+}
