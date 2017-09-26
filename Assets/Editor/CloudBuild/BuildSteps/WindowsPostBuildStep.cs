2cd2bb41030eea09d3765088258dbe821d7c54a4
Jake Huxell <darkduckmovies@gmail.com>
2017-09-26 13:55:02 +0100

    Adding default PostBuild hook

---
+++ Assets/Editor/CloudBuild/BuildSteps/WindowsPostBuildStep.cs
@@ -0,0 +1,14 @@
+﻿// Copyright Threetee Gang (C) 2017
+
+using UnityEngine;
+
+namespace Assets.Editor.CloudBuild.BuildSteps
+{
+    public static class WindowsPostBuildStep
+    {
+        public static void RunWindowsPostBuildStep()
+        {
+            Debug.Log("Post build begun");
+        }
+    }
+}
