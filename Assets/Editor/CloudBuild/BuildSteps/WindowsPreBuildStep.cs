a985ab8547bf2a99448747b0c68733b6be221ce9
Jake Huxell <darkduckmovies@gmail.com>
2017-09-26 09:15:22 +0100

    Adding static function to be called to run the windows pre-build step

---
+++ Assets/Editor/CloudBuild/BuildSteps/WindowsPreBuildStep.cs
@@ -0,0 +1,14 @@
+﻿// Copyright Threetee Gang (C) 2017
+
+using UnityEngine;
+
+namespace Assets.Editor.CloudBuild.BuildSteps
+{
+    public static class WindowsPreBuildStep
+    {
+        public static void RunWindowsPreBuildStep()
+        {
+            Debug.Log("Pre Build Step Has Begun.");
+        }
+    }
+}
