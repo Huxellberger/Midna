2feb3b5d4288ef355cf3f3904179efd11167e5cf
Jake Huxell <darkduckmovies@gmail.com>
2017-09-27 14:22:49 +0100

    Standard functions for using the dispatcher

---
+++ Assets/Editor/UnitTests/Components/UnityEvent/TestUnityMessageEventDispatcherInterface.cs
@@ -0,0 +1,24 @@
+﻿// Copyright Threetee Gang (C) 2017
+
+using Assets.Scripts.Components.UnityEvent;
+
+namespace Assets.Editor.UnitTests.Components.UnityEvent
+{
+    public class TestUnityMessageEventDispatcherInterface
+        : IUnityMessageEventInterface
+    {
+        public TestUnityMessageEventDispatcherInterface()
+        {
+            Dispatcher = new UnityMessageEventDispatcher();
+        }
+
+        // IUnityMessageEventInterface
+        public UnityMessageEventDispatcher GetUnityMessageEventDispatcher()
+        {
+            return Dispatcher;
+        }
+        // ~IUnityMessageEventInterface
+
+        public UnityMessageEventDispatcher Dispatcher { get; set; }
+    }
+}
