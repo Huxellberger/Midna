614b9a3f0626e0570b31e3fad0fe4024cbd73969
Jake Huxell <darkduckmovies@gmail.com>
2017-09-30 13:42:47 +0100

    Adding GameMode

---
+++ Assets/Editor/UnitTests/Components/GameMode/MidnaGameModeTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright Threetee Gang (C) 2017
+
+using Assets.Scripts.Components.GameMode;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Assets.Editor.UnitTests.Components.GameMode
+{
+    [TestFixture]
+    public class MidnaGameModeTests
+    {
+        [SetUp]
+        public void BeforeTest()
+        {
+            _setGameMode = null;
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            _setGameMode = null;
+            MidnaGameMode.CurrentGameMode = null;
+        }
+
+        [Test]
+        public void SetGameMode_SetGameObjectToBeGameMode()
+        {
+            var gameObject = new GameObject();
+
+            MidnaGameMode.CurrentGameMode = gameObject;
+
+            Assert.AreSame(gameObject, MidnaGameMode.CurrentGameMode);
+        }
+
+        [Test]
+        public void SetGameMode_InvokesEvent()
+        {
+            var gameObject = new GameObject();
+
+            MidnaGameMode.OnGameModeChanged += OnGameModeChangedCallback;
+            MidnaGameMode.CurrentGameMode = gameObject;
+
+            MidnaGameMode.OnGameModeChanged -= OnGameModeChangedCallback;
+            Assert.AreSame(gameObject, _setGameMode);
+        }
+
+        private void OnGameModeChangedCallback(GameObject newGameMode)
+        {
+            _setGameMode = newGameMode;
+        }
+
+        private GameObject _setGameMode;
+    }
+}
---
+++ Assets/Scripts/Components/GameMode/MidnaGameMode.cs
@@ -0,0 +1,28 @@
+﻿// Copyright Threetee Gang (C) 2017
+
+using UnityEngine;
+
+namespace Assets.Scripts.Components.GameMode
+{
+    public delegate void OnGameModeChangedDelegate(GameObject newGameMode);
+
+    public static class MidnaGameMode
+    {
+        private static GameObject _gameMode = null;
+        public static event OnGameModeChangedDelegate OnGameModeChanged;
+
+        public static GameObject CurrentGameMode
+        {
+            get { return _gameMode; }
+            set
+            {
+                _gameMode = value;
+                if (OnGameModeChanged != null)
+                {
+                    OnGameModeChanged(_gameMode);
+                }
+            }
+        }
+    }
+}
+
