d0f4147d5803e48e1970f3fabe9453e48c9f312d
Jake Huxell <darkduckmovies@gmail.com>
2017-10-02 18:59:34 +0100

    Added base HUDComponent (will add controllers next!)

---
+++ Assets/Scripts/Components/HUD/HUDComponent.cs
@@ -0,0 +1,40 @@
+﻿// Copyright Threetee Gang (C) 2017
+
+using UnityEngine;
+
+namespace Assets.Scripts.Components.HUD
+{
+    public class HUDComponent 
+        : MonoBehaviour
+    {
+        private GameObject _canvas;
+        private GameObject _canvasChild;
+        private Canvas _canvasComponent;
+
+        void Start()
+        {
+            InitialiseCanvas();
+            InitialiseElements();
+        }
+
+        void OnDestroy()
+        {
+            Destroy(_canvas);
+        }
+
+        private void InitialiseCanvas()
+        {
+            _canvas = new GameObject();
+
+            _canvasComponent = _canvas.AddComponent<Canvas>();
+            _canvasComponent.renderMode = RenderMode.ScreenSpaceOverlay;
+        }
+
+        private void InitialiseElements()
+        {
+            _canvasChild = new GameObject();
+
+            _canvasChild.transform.parent = _canvas.transform;
+        }
+    }
+}
