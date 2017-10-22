b9159fe61217a8dfd7487a24d679f2fec72dfc5c
Jake Huxell <darkduckmovies@gmail.com>
2017-10-22 23:24:31 +0100

    Last compile out I missed

--- Assets/Scripts/Test/Integration/Visual/Tests/CameraController_FadesCorrectly.cs
+++ Assets/Scripts/Test/Integration/Visual/Tests/CameraController_FadesCorrectly.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright Threetee Gang (C) 2017
 
+#if UNITY_EDITOR
+
 using Assets.Scripts.Components.Camera;
 using Assets.Scripts.Components.UnityEvent;
 using UnityEngine;
@@ -31,3 +33,5 @@ namespace Assets.Scripts.Test.Integration.Visual.Tests
         }
     }
 }
+
+#endif 
